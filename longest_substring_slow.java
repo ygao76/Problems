0940db51262df0cdcb2958365f9faeeaa41b0c6e
ygao76 <yajun.gao@gatech.edu>
2014-11-24 00:17:37 -0800

    Longest Substring Without Repeating Characters

--- longest_substring_slow.java
+++ longest_substring_slow.java
@@ -1,25 +1,35 @@
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0){
+        if(s==null||s.length()==0){
             return 0;
         }
         if(s.length()==1){
             return 1;
         }
-        HashMap<Character,Integer> substrings = new HashMap<Character, Integer>();
-        int longest = 0;
-        substrings.put(s.charAt(0),0);
+        HashMap<Character,Integer> charToIndex = new HashMap<Character, Integer>();
+        charToIndex.put(s.charAt(0),0);
+        int currLen = 1;
+        int maxLen = 0;
         for(int i=1; i<s.length();i++){
-            if(substrings.get(s.charAt(i))!=null){
-                if(substrings.size()>longest){
-                    longest = substrings.size();
+            if(charToIndex.get(s.charAt(i))!=null){
+                int repeatIndex = charToIndex.get(s.charAt(i));
+                charToIndex.put(s.charAt(i),i);
+                if((i-currLen)> repeatIndex){
+                   currLen++;
+                }else{
+                   if(maxLen<currLen){
+                      maxLen = currLen;
+                   }
+                   currLen = i - repeatIndex;
                 }
-                i=substrings.get(s.charAt(i));
-                substrings = new HashMap<Character, Integer>();
             }else{
-                substrings.put(s.charAt(i),i);
+                charToIndex.put(s.charAt(i),i);
+                currLen++;
             }
         }
-        return longest;
+        if(currLen > maxLen){
+           maxLen = currLen;
+        }
+        return maxLen;
     }
 }
